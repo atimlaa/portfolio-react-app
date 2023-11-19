eacb2c77fc38e7f9a7f7b8c112bd26a9af06f128
Aryan Timla <aryan.timla@huskers.unl.edu>
2023-11-18 20:19:53 -0600

    Updated Project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,6 +3,8 @@ import ProjectCard from "./ProjectCard";
 import ProjetConduit from "../assets/project_conduit_logo.png";
 import pyP3DB from "../assets/pyp3db_logo.png";
 import TBF from "../assets/tbf_logo.png";
+import SuperBowl from "../assets/super_bowl.png";
+import ReportDashboard from "../assets/report_dashboard.png";
 
 const Project = () => {
   return (
@@ -27,6 +29,27 @@ const Project = () => {
 
           Tech Stack:
           Apache Airflow, Python, R, PySpark, Google BigQuery, Google Cloud Storage, Google DataProc, Google Data Studio, Shiny, and many more`}
+          />
+          <ProjectCard
+            title="Monthly Report App"
+            imageSrc={ReportDashboard}
+            description={`Report Automation Dashboard
+          
+            This project aims to enhance the efficiency of Nebraska Public Media's monthly reporting process that delivers a holistic view of KPIs and metrics by developing an integrated dashboard.
+            
+            This will consolidate data from diverse platforms, including the Website, Mobile Apps, News, TV, Radio and PBS Passport audience,
+            integrating data sources through API connections, database queries, and advanced data extraction scripts. 
+
+            Tech Stack: R, Shiny, Golem, Bootstrap, Google BigQuery, Google Cloud Storage, renv, Github Actions and many more`}
+          />
+          <ProjectCard
+            title="Super Bowl Data Analysis"
+            imageSrc={SuperBowl}
+            description={`Data Analysis Project
+
+            This data science project delves into the Super Bowl, generating insights into game outcomes, viewership, and halftime shows.
+            
+            Tech Stack: Python (pandas, pyplot, seaborn), Streamlit`}
           />
           <ProjectCard
             title="pyP3DB"
